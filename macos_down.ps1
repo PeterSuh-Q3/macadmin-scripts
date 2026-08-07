ccd362f21c7b06d085351e9dc37a470019e15a9a
GitHub Action <action@github.com>
2026-08-07 02:50:05 +0000

    Update macOS versions and scripts

--- macos_down.ps1
+++ macos_down.ps1
@@ -38,10 +38,11 @@ $options = @(
     "macOS Big Sur (11.7.10)",
     "macOS Monterey (12.7.6)",
     "macOS Ventura (13.7.8)",
-    "macOS Sonoma (14.8.8)",
-    "macOS Sequoia (15.7.8)",
-    "macOS Tahoe (26.6)",
+    "macOS Sonoma (14.8.9)",
+    "macOS Sequoia (15.7.9)",
+    "macOS Tahoe (26.6.1)",
     "Exit"
+)
 )
 )
 )
